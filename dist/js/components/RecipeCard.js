7777ad72e97c6b1d3f514f65453dcd62efcbe591
Angel <angeldanielmero2003@gmail.com>
2023-05-03 02:48:24 +0200

    _nav.scss, index.html, _buttons.scss, Recipecard.js, main.js

--- dist/js/components/RecipeCard.js
+++ dist/js/components/RecipeCard.js
@@ -5,29 +5,21 @@ app.component('recipe-card', {
         image: {
             type: String
         },
-        category: {
-            type: String,
-            default: "recipe category"
-        },
         name: {
             type: String,
             default: "recipe name"
-        },
-        description: {
-            type: String,
-            default: "recipe description"
         },
         time: {
             type: String,
             default: "recipe time"
         },
+        portions: {
+            type: String,
+            default: "recipe portions"
+        },
         level: {
             type: String,
             default: "recipe level"
-        },
-        likes: {
-            type: Number,
-            default: 10
         }
     },
 
@@ -42,17 +34,12 @@ app.component('recipe-card', {
         `<div class="card">
             <img v-bind:src="image" class="card-img-top" alt="featured-recipe">
             <div class="card-body">
-                <p>{{ category }}</p>
-                <h5 class="card-title">{{ name }}</h5>
-                <p>{{ description }}</p>
+                <h3 class="card-title">{{ name }}</h5>
                 <p>{{ time }}</p>
+                <p>{{ portions }}</p>
                 <p>{{ level }}</p>
-                <p>{{ likes }}</p>
 
-                <button class="btn btn-danger">Like</button>
-                <button class="btn btn-dark">Dislike</button>
                 <button class="btn btn-primary">View Recipe</button>
-                <!-- v-on:click="onClickLike"// evento al que llama en el main js -->
             </div>
         </div>`
 })
