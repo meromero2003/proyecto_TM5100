3ed3e89f31f7121b63bb4acec8137ae29dca6bf8
Angel <angeldanielmero2003@gmail.com>
2023-05-29 01:03:11 +0200

    recipe category corrections

--- dist/js/components/RecipeCategory.js
+++ dist/js/components/RecipeCategory.js
@@ -14,8 +14,8 @@ app.component('recipe-category', {
     template:
         /*html*/
         `
-        <li class="list-group-item"><a class="list-link" href="#"><i
-        class="fa-solid fa-egg me-3" v-on:click="onClickCategory"></i>{{name}}</a> </li>
+        <li class="list-group-item" v-on:click="onClickCategory"><a class="list-link"><i
+        class="fa-solid fa-egg me-3" ></i>{{name}}</a> </li>
             
         `
 })
